be367364129a827b4542c1a3ffdce41700c9e8b4
Bryan <bryan.cedeo@gmail.com>
2021-01-16 20:16:45 -0500

    segundo

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,7 +4,22 @@ export const useFetch = (initialUrl)=>{
     const [url,setUrl] = useState(initialUrl);
     const [data,setData] = useState({});
     useEffect(()=>{
-        fetch(url)
+        fetch(url,{
+            method: 'GET',
+            mode:'cors',
+            // body: JSON.stringify(data),
+            // headers :{
+            //   'Content-Type': 'application/json',
+            //   'Access-Control-Allow-Origin':'*'
+            // },
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin':'*',
+              'Access-Control-Allow-Headers':'Origin,Content-Type,Accept',
+        
+            }
+          })
             .then((res)=>{
                 return res.json()
             })
